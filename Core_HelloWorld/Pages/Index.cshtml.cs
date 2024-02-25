e883877264459a4a8133f93ad37105adc100d237
vikram <vik_brlee@yahoo.com>
2024-02-25 17:05:40 +0530

    My Third Commit

--- Core_HelloWorld/Pages/Index.cshtml.cs
+++ Core_HelloWorld/Pages/Index.cshtml.cs
@@ -12,7 +12,7 @@ namespace Core_HelloWorld.Pages
         public string Message { get; set; }
         public void OnGet()
         {
-            this.Message = "This is my First ASP.Net Core Razor Page";
+            this.Message = "This is my First ASP.Net Core Vikram Page";
         }
     }
 }
